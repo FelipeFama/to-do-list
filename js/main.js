607c14b28b9caf15a95518ea77d936ec45b00223
Felipe <91050670+FelipeFama@users.noreply.github.com>
2022-10-04 23:11:49 -0300

    changes

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+const editForm = document.querySelector('#edit-form');
+const editInput = document.querySelector('#edit-input');
+const cancelEditBtn = document.querySelector('#cancel-edit-btn');
+const searchInput = document.querySelector('#search-input');
+const eraseBtn = document.querySelector('#erase-button');
+const filterBtn = document.querySelector('#filter-select');
+
+let oldInputValue;
+
+//Functions
+
+//LocalStorage Data
+
+//Events
